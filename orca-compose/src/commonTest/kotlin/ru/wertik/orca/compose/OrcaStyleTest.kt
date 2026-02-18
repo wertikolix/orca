26d13959ec348918d0f166427e8f8628db25340b
kosatka <maggisttr33@gmail.com>
2026-02-18 02:57:00 +0000

    test: add OrcaDefaults dark and light style coverage

--- orca-compose/src/commonTest/kotlin/ru/wertik/orca/compose/OrcaStyleTest.kt
+++ orca-compose/src/commonTest/kotlin/ru/wertik/orca/compose/OrcaStyleTest.kt
@@ -2,6 +2,7 @@ package ru.wertik.orca.compose
 
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
 
 class OrcaStyleTest {
 
@@ -40,5 +41,72 @@ class OrcaStyleTest {
         assertEquals(style.table.maxColumnWidth, style.tableMaxColumnWidth)
         assertEquals(style.table.autoColumnCharacterWidth, style.tableAutoColumnCharacterWidth)
         assertEquals(style.table.fillAvailableWidth, style.tableFillAvailableWidth)
+    }
+}
+
+class OrcaDefaultsTest {
+
+    @Test
+    fun lightStyleIsDefaultOrcaStyle() {
+        assertEquals(OrcaStyle(), OrcaDefaults.lightStyle())
+    }
+
+    @Test
+    fun darkStyleHasDifferentTypographyColorsThanLightStyle() {
+        val light = OrcaDefaults.lightStyle()
+        val dark = OrcaDefaults.darkStyle()
+
+        assertNotEquals(light.typography.heading1.color, dark.typography.heading1.color)
+        assertNotEquals(light.typography.paragraph.color, dark.typography.paragraph.color)
+    }
+
+    @Test
+    fun darkStyleHasDifferentCodeBackgroundThanLightStyle() {
+        val light = OrcaDefaults.lightStyle()
+        val dark = OrcaDefaults.darkStyle()
+
+        assertNotEquals(light.code.background, dark.code.background)
+    }
+
+    @Test
+    fun darkStyleHasDifferentLinkColorThanLightStyle() {
+        val light = OrcaDefaults.lightStyle()
+        val dark = OrcaDefaults.darkStyle()
+
+        assertNotEquals(light.inline.link.color, dark.inline.link.color)
+    }
+
+    @Test
+    fun darkStyleHasDifferentTableBorderColorThanLightStyle() {
+        val light = OrcaDefaults.lightStyle()
+        val dark = OrcaDefaults.darkStyle()
+
+        assertNotEquals(light.table.borderColor, dark.table.borderColor)
+    }
+
+    @Test
+    fun darkStyleHeadingLevelClamping() {
+        val dark = OrcaDefaults.darkStyle()
+
+        assertEquals(dark.typography.heading1, dark.heading(1))
+        assertEquals(dark.typography.heading6, dark.heading(6))
+        assertEquals(dark.typography.heading1, dark.heading(-1))
+        assertEquals(dark.typography.heading6, dark.heading(99))
+    }
+
+    @Test
+    fun darkStyleCompatibilityAccessorsMirrorGroupedStyles() {
+        val dark = OrcaDefaults.darkStyle()
+
+        assertEquals(dark.typography.paragraph, dark.paragraph)
+        assertEquals(dark.inline.inlineCode, dark.inlineCode)
+        assertEquals(dark.inline.link, dark.linkStyle)
+        assertEquals(dark.code.text, dark.codeBlock)
+        assertEquals(dark.code.background, dark.codeBlockBackground)
+        assertEquals(dark.code.borderColor, dark.codeBlockBorderColor)
+        assertEquals(dark.quote.stripeColor, dark.quoteStripeColor)
+        assertEquals(dark.table.borderColor, dark.tableBorderColor)
+        assertEquals(dark.thematicBreak.color, dark.thematicBreakColor)
+        assertEquals(dark.image.background, dark.imageBackground)
     }
 }
